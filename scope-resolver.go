e5e660256358ced8dd9a3245dd9baecbf9fd35df
Steve Campbell <sdcampbell68@live.com>
2024-01-11 08:52:49 -0500

    Update scope-resolver.go
    Added checking for hostnames in TLS cert SAN

--- scope-resolver.go
+++ scope-resolver.go
@@ -2,83 +2,144 @@ package main
 
 import (
 	"bufio"
+	"context"
+	"crypto/tls"
 	"flag"
 	"fmt"
+	"log"
 	"net"
+	"net/http"
 	"os"
-	"regexp"
+	"strings"
+	"sync"
+	"time"
 )
 
-// Function to check if the address is an IP address
-func isIP(addr string) bool {
-	return net.ParseIP(addr) != nil
-}
+// Grabs the SSL certificate, then dumps the SAN and CommonName
+func sslChecks(ip string, resChan chan<- string, client *http.Client) {
 
-// Function to process each line of the input
-func processLine(line string) {
-	ipRegex := regexp.MustCompile(`\b(?:\d{1,3}\.){3}\d{1,3}\b`)
-	if ipRegex.MatchString(line) && isIP(line) {
-		// It's an IP address, attempt a reverse DNS lookup
-		names, err := net.LookupAddr(line)
-		if err == nil && len(names) > 0 {
-			//trimmed := strings.TrimSuffix(names[0], ".")
-			fmt.Printf("%s\t(%s)\n", line, names) // Print the resolved hostname
-		} else {
-			fmt.Println(line) // If reverse lookup fails, print the IP address
+	url := ip
+
+	// make sure we use https as we're doing SSL checks
+	if strings.HasPrefix(ip, "http://") {
+		url = strings.Replace(ip, "http://", "https://", 1)
+	} else if !strings.HasPrefix(ip, "https://") {
+		url = "https://" + ip
+	}
+
+	req, reqErr := http.NewRequest("HEAD", url, nil)
+	if reqErr != nil {
+		return
+	}
+
+	resp, clientErr := client.Do(req)
+	if clientErr != nil {
+		return
+	}
+
+	if resp.TLS != nil && len(resp.TLS.PeerCertificates) > 0 {
+		dnsNames := resp.TLS.PeerCertificates[0].DNSNames
+		for _, name := range dnsNames {
+			resChan <- "[SSL-SAN] " + ip + " " + string(name)
 		}
-	} else {
-		// It's a hostname, print it
-		fmt.Println(line)
+		resChan <- "[SSL-CN] " + ip + " " + resp.TLS.PeerCertificates[0].Subject.CommonName
 	}
 }
 
-// Prints the usage of the program
-func printUsage() {
-	fmt.Printf("Usage of %s:\n", os.Args[0])
-	fmt.Println("  (no arguments)         Read from stdin")
-	fmt.Println("  <file_path>            Read from the specified file")
-	fmt.Println("  -h or --help           Print this help message")
-	fmt.Println("\nExamples:")
-	fmt.Printf("  cat ips.txt | %s\n", os.Args[0])
-	fmt.Printf("  %s ips.txt\n", os.Args[0])
+// Do a DNS PTR lookup on the IP
+func dnsChecks(ip string, resChan chan<- string, resolver *net.Resolver) {
+
+	addr, err := resolver.LookupAddr(context.Background(), ip)
+	if err != nil {
+		return
+	}
+
+	for _, a := range addr {
+		resChan <- "[DNS-PTR] " + ip + " " + a
+	}
 }
 
-func main() {
-	helpFlag := flag.Bool("help", false, "Print help information")
-	flag.BoolVar(helpFlag, "h", false, "Print help information (shorthand)")
+func worker(jobChan <-chan string, resChan chan<- string, wg *sync.WaitGroup, transport *http.Transport, client *http.Client, resolver *net.Resolver) {
+	defer wg.Done()
 
+	for job := range jobChan {
+		sslChecks(job, resChan, client)
+		dnsChecks(job, resChan, resolver)
+	}
+
+}
+func main() {
+	workers := flag.Int("t", 32, "numbers of threads")
+	resolverIP := flag.String("r", "", "IP of DNS resolver for lookups")
+	dnsProtocol := flag.String("protocol", "udp", "Protocol for DNS lookups (tcp or udp)")
+	resolverPort := flag.Int("p", 53, "Port to bother the specified DNS resolver on")
 	flag.Parse()
 
-	// If help flag is present or no arguments are provided, print usage and exit
-	if *helpFlag || (len(os.Args) == 1 && len(flag.Args()) == 0) { // No input provided
-		fi, _ := os.Stdin.Stat() // Get the FileInfo struct describing the standard input.
-		if (fi.Mode() & os.ModeCharDevice) != 0 {
-			printUsage()
-			return
+	scanner := bufio.NewScanner(os.Stdin)
+	jobChan := make(chan string)
+	resChan := make(chan string)
+	done := make(chan struct{})
+
+	// Set up TLS transport
+	var transport = &http.Transport{
+		Dial: (&net.Dialer{
+			Timeout: 5 * time.Second,
+		}).Dial,
+		TLSHandshakeTimeout: 5 * time.Second,
+		TLSClientConfig:     &tls.Config{InsecureSkipVerify: true},
+	}
+
+	// Set up HTTP client
+	var client = &http.Client{
+		Timeout:   time.Second * 10,
+		Transport: transport,
+	}
+
+	// Set up DNS resolver
+	var resolver *net.Resolver
+
+	if *resolverIP != "" {
+		resolver = &net.Resolver{
+			PreferGo: true,
+			Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+				d := net.Dialer{}
+				return d.DialContext(ctx, *dnsProtocol, fmt.Sprintf("%s:%d", *resolverIP, *resolverPort))
+			},
 		}
 	}
 
-	var scanner *bufio.Scanner
+	var wg sync.WaitGroup
+	wg.Add(*workers)
 
-	if len(flag.Args()) > 0 {
-		filePath := flag.Arg(0) // Get the file path from the command-line argument
-		file, err := os.Open(filePath)
-		if err != nil {
-			fmt.Printf("Error opening file: %v\n", err)
-			return
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+
+	for i := 0; i < *workers; i++ {
+
+		go worker(jobChan, resChan, &wg, transport, client, resolver)
+	}
+
+	go func() {
+		for scanner.Scan() {
+			jobChan <- scanner.Text()
 		}
-		defer file.Close()
-		scanner = bufio.NewScanner(file)
-	} else {
-		// Read from stdin
-		scanner = bufio.NewScanner(os.Stdin)
-	}
+		if err := scanner.Err(); err != nil {
+			log.Println(err)
+		}
+		close(jobChan)
+	}()
 
-	for scanner.Scan() {
-		processLine(scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Printf("Error while reading: %v\n", err)
+	for {
+		select {
+		case <-done:
+			return
+		case res := <-resChan:
+			if strings.HasSuffix(res, ".") {
+				res = res[:len(res)-1]
+			}
+			fmt.Println(res)
+		}
 	}
 }
